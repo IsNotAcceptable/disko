b3f20c1fcee4d00640aab7cfd0061fa80c8a8a4e
Serj <fil.sergey2008@gmail.com>
2025-10-05 05:11:55 +0000

    My config
    Config with swap and BTRFS fs

---
+++ disko.nix
@@ -0,0 +1,39 @@
+{
+  disko.devices = {
+    disk = {
+      my-disk = {
+        device = "/dev/sdb";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+	    swap = {
+	      size = "8G";
+	      type = "8200";
+	      content = {
+        	type = "swap";
+	      };
+	    };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "btrfs";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
